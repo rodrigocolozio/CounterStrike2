e65945a0265115a07a948c98231a57b5fa052975
Rdorigo Colozio <rodrigocolozio@gmail.com>
2023-05-13 12:22:30 -0300

    Initial Commit

---
+++ CS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CS2
+//
+//  Created by Rodrigo Colozio on 13/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
